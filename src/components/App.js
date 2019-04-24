c4db11da61c7762df0d4176f0c5cc7c3ebdd422b
Saint-Jean Germila <germila@noukod.com>
2019-04-24 19:03:27 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -16,12 +16,39 @@ class App extends React.Component {
     }
   }
 
+  //update type of pet
   onChangeType = (event) => {
     this.setState({
       filters: {
         type: event.target.value
       }
     })
+  }
+
+  onAdoptPet = (petID) => {
+    const pets = this.state.pets.map(pet => {
+      return pet.id === petID ? {...pet, isAdopted: true} : pet;
+    })
+    this.setState({
+      pets: pets
+    })
+  }
+
+//fetches correct array of pets depending on type chosen
+//data in lines 48-49 is the list of pets
+  onFindPetsClick = () => {
+    let indexUrl = '/api/pets'
+
+    if (this.state.filters.type !== 'all') {
+      indexUrl += `?type=${this.state.filters.type}`
+    }
+
+    fetch(indexUrl)
+      .then(response => response.json())
+      .then(data => this.setState({
+        pets: data,
+        ...this.state.filters
+    }))
   }
 
   render() {
@@ -33,10 +60,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
